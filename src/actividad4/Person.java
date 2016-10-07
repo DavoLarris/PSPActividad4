421592cb38dfe350734fc9ec79381d5965259645
DavoLarris <davo.larris@hotmail.com>
2016-10-07 10:06:30 +0200

    Person aggregated jejejej

---
+++ src/actividad4/Person.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actividad4;
+
+/**
+ *
+ * @author DavoLarris
+ */
+class Person {
+
+//TODO: Declare the attributes of a person: age and dni 
+    private int age;
+    private String dni;
+    
+    /**
+     * Class constructor.
+     */
+    public Person() {
+//Initialize attributes to random values.The age must be an integer 
+//between 0 and 100, and the dni a 8 characters alphanumeric string 
+
+    }
+
+    @Override
+    public String toString() {
+//TODO: Return a string with the attributes of a Person to be 
+//printed in the file 
+        return "";
+    }
+
+}
